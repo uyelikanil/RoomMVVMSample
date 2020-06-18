ddce5dc3f5e7c5c35fe2d1d4c71158391d3489fb
ANIL YILMAZ <uyelikanil@gmail.com>
2020-06-18 11:19:27 +0300

    New word activity created

---
+++ app/src/main/java/com/anilyilmaz/roommvvmsample/NewWordActivity.java
@@ -0,0 +1,14 @@
+package com.anilyilmaz.roommvvmsample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewWordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_word);
+    }
+}
