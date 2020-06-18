2728c678f5f8019f13fb6603846846ef78f6bac0
ANIL YILMAZ <uyelikanil@gmail.com>
2020-06-18 09:27:57 +0300

    Words getter changed as live data

--- app/src/main/java/com/anilyilmaz/roommvvmsample/WordDao.java
+++ app/src/main/java/com/anilyilmaz/roommvvmsample/WordDao.java
@@ -1,5 +1,6 @@
 package com.anilyilmaz.roommvvmsample;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
@@ -18,5 +19,5 @@ public interface WordDao {
     void deleteAll();
 
     @Query("SELECT * from word_table ORDER BY word ASC")
-    List<Word> getAlphabetizedWords();
+    LiveData<List<Word>> getAlphabetizedWords();
 }
